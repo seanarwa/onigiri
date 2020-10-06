e59394012d347d737bd639dd51e082eabc2a791a
Sean Chang <sean.chang@idemia.com>
2020-10-05 17:14:11 -0700

    Added source code

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:onigiri/pages/index.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Onigiri',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => HomePage(),
+        '/login': (BuildContext context) => LoginPage(),
+      },
+    );
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:onigiri/auth.dart';
+import 'package:onigiri/pages/index.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  static final String routeName = '/';
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Auth.authState().listen((user) {
+      if (user == null) {
+        print("User is not signed in, redirecting to LoginPage ...");
+        Navigator.of(context).pushReplacementNamed(LoginPage.routeName);
+        return;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              child: Text("Logout"),
+              onPressed: Auth.signOut,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
