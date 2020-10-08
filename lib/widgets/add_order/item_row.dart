4c6e9255c0ec8c8dd99983ebe7aab554195c6c7b
Sean Chang <sean.chang@idemia.com>
2020-10-08 12:32:33 -0700

    Fix add order ui error

--- lib/widgets/add_order/item_row.dart
+++ lib/widgets/add_order/item_row.dart
@@ -23,8 +23,8 @@ class _ItemRowState extends State<ItemRow> {
 
   @override
   void initState() {
-    _onChanged = widget.onChanged ?? () {};
-    _items = List.from(widget.items);
+    _onChanged = widget.onChanged ?? (Item item) {};
+    _items = List.from(widget.items ?? []);
     _index = _indexItems(_items);
     super.initState();
   }
@@ -43,30 +43,28 @@ class _ItemRowState extends State<ItemRow> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        DropdownSearch<Item>(
-          label: "Select item",
-          onFind: (String filter) async {
-            if (filter.isEmpty) {
-              return _items;
-            }
+    return ListTile(
+      leading: DropdownSearch<Item>(
+        label: "Select item",
+        onFind: (String filter) async {
+          if (filter.isEmpty) {
+            return _items;
+          }
 
-            List<Item> result = [];
-            filter = filter.toLowerCase();
-            List<String> searchTerms = _index.keys.toList();
-            for (String searchTerm in searchTerms) {
-              searchTerm = searchTerm.toLowerCase();
-              if (searchTerm.contains(filter)) {
-                result.addAll(_index[searchTerm]);
-              }
+          List<Item> result = [];
+          filter = filter.toLowerCase();
+          List<String> searchTerms = _index.keys.toList();
+          for (String searchTerm in searchTerms) {
+            searchTerm = searchTerm.toLowerCase();
+            if (searchTerm.contains(filter)) {
+              result.addAll(_index[searchTerm]);
             }
-            return result;
-          },
-          itemAsString: (Item item) => item.name,
-          onChanged: _onChanged,
-        ),
-      ],
+          }
+          return result;
+        },
+        itemAsString: (Item item) => item.name,
+        onChanged: _onChanged,
+      ),
     );
   }
 }
